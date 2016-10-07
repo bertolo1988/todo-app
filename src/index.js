c6ff7714e868b3def4568d619ed2780a7fc4c717
Tiago Bértolo <tiagobertolo@gmail.com>
2016-10-07 12:05:43 +0100

    uploaded a built page

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoApp from './TodoApp';
-import './index.css';
-
-ReactDOM.render(
-    <TodoApp />,
-    document.getElementById('root')
-);
